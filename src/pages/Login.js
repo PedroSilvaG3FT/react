cce0c8afbb5b5b624a0c3a32b9d02f358a764f31
Pedro <pedro.silva-dev@hotmail.com>
2019-08-29 23:48:50 -0300

    iniciando html de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,5 +3,9 @@ import logo from "../assets/img/security.svg";
 import "./Login.css";
 
 export default function Login() {
-  return <img src={logo} alt="Login" />;
+  return (
+    <div className="login-container">
+      <img src={logo} alt="Login" />;
+    </div>
+  );
 }
