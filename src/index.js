b001d3e8052bebb179a5d26dcb43c7ba46fc17fe
Pedro <pedro.silva-dev@hotmail.com>
2019-08-29 23:12:53 -0300

    Removendo arquivos para um inicio limpo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom"; //Biblioteca que referencia a plataforma que o react vai rodar
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Preenche a div root do arquivo index.html com o arquivo APP
+ReactDOM.render(<App />, document.getElementById("root"));
