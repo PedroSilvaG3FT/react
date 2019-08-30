cee7181b54c4de5bc20f197d69c3dbcbf75efaed
Pedro <pedro.silva-dev@hotmail.com>
2019-08-29 23:26:43 -0300

    Como importar imagens

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import logo from "./assets/img/security.svg";
 
 function App() {
-  return <h1>Hello World !</h1>;
+  return (
+    // <h1>Hello World !</h1>
+    <img src={logo} alt="Login" />
+  );
 }
 
 export default App;
