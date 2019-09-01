5f95e46a870ddececf724d13579fdb7b6df9a880
Pedro <pedro.silva-dev@hotmail.com>
2019-08-31 22:52:59 -0300

    Preparando input para receber um valor

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,11 +3,16 @@ import logo from "../assets/img/security.svg";
 import "./Login.css";
 
 export default function Login() {
+  const [userName, setUsername] = useState("");
   return (
     <div className="login-container">
       <form>
         <img src={logo} alt="Login" />
-        <input placeholder="Digite seu usuário do GitHub " />
+        <input
+          placeholder="Digite seu usuário do GitHub "
+          value={userName}
+          onChange={e => setUsername(e.target.value)}
+        />
         <button type="submit ">Enviar</button>
       </form>
     </div>
