5933b8498378dae3d8b36870f230cc49318bf810
Pedro <pedro.silva-dev@hotmail.com>
2019-08-31 22:59:47 -0300

    Controle do formulario | capturando valores

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,9 +4,16 @@ import "./Login.css";
 
 export default function Login() {
   const [userName, setUsername] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault(); //Previne redirecionamento para outra pagina
+
+    console.log(userName);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Login" />
         <input
           placeholder="Digite seu usuário do GitHub "
