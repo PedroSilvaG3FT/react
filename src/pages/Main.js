853cbf63331dd1c8f3b35ca5fa8d910670c55f82
Pedro <walme@LAPTOP-13PJ4Q0V>
2019-09-04 00:55:40 -0300

    Iniciando css

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Main.css";
 import logo from "../assets/img/security.svg";
 import like from "../assets/img/like.svg";
 import dislike from "../assets/img/close.svg";
@@ -9,6 +10,90 @@ export default function Main({ match }) {
       <img src={logo} alt="Tindev" />
 
       <ul>
+        <li>
+          <img
+            src="https://i.pinimg.com/originals/12/73/c3/1273c3b6276f28b68aa0276275a58275.jpg "
+            alt=""
+          />
+          <footer>
+            <strong>Pedro Silva</strong>
+            <p>Teste de campo da descrição</p>
+          </footer>
+
+          <div className="buttons ">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://i.pinimg.com/originals/12/73/c3/1273c3b6276f28b68aa0276275a58275.jpg "
+            alt=""
+          />
+          <footer>
+            <strong>Pedro Silva</strong>
+            <p>Teste de campo da descrição</p>
+          </footer>
+
+          <div className="buttons ">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://i.pinimg.com/originals/12/73/c3/1273c3b6276f28b68aa0276275a58275.jpg "
+            alt=""
+          />
+          <footer>
+            <strong>Pedro Silva</strong>
+            <p>Teste de campo da descrição</p>
+          </footer>
+
+          <div className="buttons ">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://i.pinimg.com/originals/12/73/c3/1273c3b6276f28b68aa0276275a58275.jpg "
+            alt=""
+          />
+          <footer>
+            <strong>Pedro Silva</strong>
+            <p>Teste de campo da descrição</p>
+          </footer>
+
+          <div className="buttons ">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
         <li>
           <img
             src="https://i.pinimg.com/originals/12/73/c3/1273c3b6276f28b68aa0276275a58275.jpg "
