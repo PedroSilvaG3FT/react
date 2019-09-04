c71337705b741952dec1f34612c36459bb93ef2e
Pedro <walme@LAPTOP-13PJ4Q0V>
2019-09-04 00:32:49 -0300

    Iniciando HTML da pagina

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,35 @@
 import React from "react";
 import logo from "../assets/img/security.svg";
+import like from "../assets/img/like.svg";
+import dislike from "../assets/img/close.svg";
 
 export default function Main({ match }) {
-  return <h1>{match.params.id}</h1>;
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+
+      <ul>
+        <li>
+          <img
+            src="https://i.pinimg.com/originals/12/73/c3/1273c3b6276f28b68aa0276275a58275.jpg "
+            alt=""
+          />
+          <footer>
+            <strong>Pedro Silva</strong>
+            <p>Teste de campo da descrição</p>
+          </footer>
+
+          <div className="buttons ">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
